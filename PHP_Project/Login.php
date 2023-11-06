b6265ee9c6ffd958ee22b497a1f400ba6dc8116d
ZeAlpacalypse <116012031+ZeAlpacalypse@users.noreply.github.com>
2023-11-05 20:18:19 -0400

    Update Login.php

--- PHP_Project/Login.php
+++ PHP_Project/Login.php
@@ -21,55 +21,6 @@
             <button id="LoginButton">Login</button>
         </div>
 
-        <!--<div id="AddUpdatePanel" class="hidden">
-            <div class="inputContainer">
-                <div class="inputLabel">Game ID:</div>
-                <div class="inputField">
-                    <input id="gameID" name="gameID" type="number" min="100" max="999" />
-                </div>
-            </div>
-            <div class="inputContainer">
-                <div class="inputLabel">Age Rating:</div>
-                <div class="inputField">
-                    <select id="ageRatingID" name="ageRatingID">-->
-        <!-- to be filled in by JS -->
-        <!--</select>
-                </div>
-            </div>
-            <div class="inputContainer">
-                <div class="inputLabel">Title:</div>
-                <div class="inputField">
-                    <input id="title" name="title" />
-                </div>
-            </div>
-            <div class="inputContainer">
-                <div class="inputLabel">Price:</div>
-                <div class="inputField">
-                    <input id="price" type="number" name="price" min="0" />
-                </div>
-            </div>
-            <div class="inputContainer">
-                <div class="inputLabel">Digital:</div>
-                <div class="inputField">
-                    <input id="digital" type="checkbox" name="digital" />
-                </div>
-            </div>
-            <div class="inputContainer">
-                <div class="inputLabel">&nbsp;</div>
-                <div class="inputField">
-                    <button id="DoneButton" class="btn-primary">
-                        Done
-                    </button>
-                    <button id="CancelButton" class="btn-primary">
-                        Cancel
-                    </button>
-                </div>
-            </div>
-        </div>
-
-        <div id="VideoGamesTable">-->
-        <!-- to be filled in by JS -->
-        <!--</div>-->
     </div>
 </body>
 
