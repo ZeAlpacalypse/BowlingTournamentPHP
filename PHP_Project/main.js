c39559e31542d21d73753a0ea9ebf8ac42bc5954
ZeAlpacalypse <116012031+ZeAlpacalypse@users.noreply.github.com>
2023-12-01 13:24:11 -0400

    Create main.js
    started login on main.js

---
+++ PHP_Project/main.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  document.querySelector("#LoginButton").addEventListener("click", userLogin);
+};
+function userLogin() {
+  let username = document.querySelector("#username");
+  let password = document.querySelector("#password");
+}
