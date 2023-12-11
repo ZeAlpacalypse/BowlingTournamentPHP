f83b857ab05e5560b0a629ab13c9893d27b7774b
ZeAlpacalypse <116012031+ZeAlpacalypse@users.noreply.github.com>
2023-12-11 16:25:20 -0400

    basic navigation and login
    ready to add database api

--- PHP_Project/main.js
+++ PHP_Project/main.js
@@ -2,6 +2,14 @@ window.onload = function () {
   document.querySelector("#loginButton").addEventListener("click", userLogin);
 };
 function userLogin() {
-  alert("logged in");
-  location.replace("guestDashboard.html");
+  let username = document.querySelector("#username").value;
+  let password = document.querySelector("#password").value;
+
+  if (username === "guest") {
+    location.replace("guestDashboard.html");
+  } else if (username === "scorekeeper") {
+    location.replace("scorekeeperDashboard.html");
+  } else if (username === "admin") {
+    location.replace("adminDashboard.html");
+  } else alert("Not logged in!");
 }
