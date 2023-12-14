f7dff42e076bd8bb4f5d457af06b13d9f80a1b63
ZeAlpacalypse <116012031+ZeAlpacalypse@users.noreply.github.com>
2023-12-14 05:59:21 -0400

    game recap

--- PHP_Project/guest.js
+++ PHP_Project/guest.js
@@ -1,21 +1,37 @@
 window.onload = function () {
   document.getElementById("viewTeamsBtn").addEventListener("click", showTeams);
   document
-    .getElementById("viewStandingBtn")
-    .addEventListener("click", showStandings);
+    .getElementById("viewTeamInfo")
+    .addEventListener("click", showTeamInfo);
+  document.getElementById("gameRecap").addEventListener("click", showRecap);
 };
 
 function showTeams() {
+  clearSelections();
   document.querySelector(".teams").classList.add("visible");
-  document.querySelector(".standings").classList.remove("visible");
+  document.querySelector(".players").classList.remove("visible");
 
   getAllTeams();
-
+  document
+    .querySelector("#teamsTable")
+    .addEventListener("click", handleRowClick);
 }
 
-function showStandings() {
+function showTeamInfo() {
   document.querySelector(".teams").classList.remove("visible");
-  document.querySelector(".standings").classList.add("visible");
+  document.querySelector(".players").classList.add("visible");
+  getAllPlayers();
+  document
+    .querySelector("#playersTable")
+    .addEventListener("click", handleRowClick);
+}
+function showRecap() {
+  document.querySelector(".players").classList.remove("visible");
+  document.querySelector(".gameRecap").classList.add("visible");
+  getAllGames();
+  document
+    .querySelector("#gameRecapTable")
+    .addEventListener("click", showRecap);
 }
 
 function getAllTeams() {
@@ -28,6 +44,45 @@ function getAllTeams() {
       if (xhr.status === 200) {
         if (resp.data) {
           buildTable(resp.data);
+          setButtonState(true);
+        } else if (xhr.status === 500) {
+          alert("Server Error: " + resp.error);
+        }
+      }
+    }
+  };
+  xhr.open(method, url, true);
+  xhr.send();
+}
+function getAllPlayers() {
+  let url = "playerService/players";
+  let method = "GET";
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      let resp = JSON.parse(xhr.responseText);
+      if (xhr.status === 200) {
+        if (resp.data) {
+          buildTeam(resp.data);
+        } else if (xhr.status === 500) {
+          alert("Server Error: " + resp.error);
+        }
+      }
+    }
+  };
+  xhr.open(method, url, true);
+  xhr.send();
+}
+function getAllGames() {
+  let url = "gameService/games";
+  let method = "GET";
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      let resp = JSON.parse(xhr.responseText);
+      if (xhr.status === 200) {
+        if (resp.data) {
+          buildGames(resp.data);
         } else if (xhr.status === 500) {
           alert("Server Error: " + resp.error);
         }
@@ -47,11 +102,72 @@ function buildTable(text) {
     html += "<tr>";
     html += "<td>" + row.teamID + "</td>";
     html += "<td>" + row.teamName + "</td>";
-    html += "<td><p>View Team</p><p>Edit Team</p></td>";
     html += "</tr>";
   }
   html += "</table>";
   let theTable = document.querySelector("#teamsTable");
   theTable.innerHTML = html;
 }
+function buildTeam(text) {
+  let arr = JSON.parse(text);
+  let row = document.querySelector(".selected");
+  let id = Number(row.querySelectorAll("td")[0].innerHTML);
+  let html =
+    "<table><tr><th>Player ID</th><th>Player Name</th><th>Hometown</th><th>Province</th></tr>";
+  for (let i = 0; i < arr.length; i++) {
+    let player = arr[i];
+    if (player.teamID === id) {
+      html += "<tr>";
+      html += "<td>" + player.playerID + "</td>";
+      html += "<td>" + player.firstName + " " + player.lastName + "</td>";
+      html += "<td>" + player.homeTown + "</td>";
+      html += "<td>" + player.provinceCode + "</td>";
+      html += "</tr>";
+    }
+  }
+  html += "</table>";
+  let theTable = document.querySelector("#playersTable");
+  theTable.innerHTML = html;
+}
+function buildGames(text) {
+  let arr = JSON.parse(text);
+  let row = document.querySelector(".selected");
+  let id = Number(row.querySelectorAll("td")[0].innerHTML);
+  console.log(id);
+  let html =
+    "<table><tr><th>Game ID</th><th>Player ID</th><th>Score</th><th>Game State</th></tr>";
+  for (let i = 0; i < arr.length; i++) {
+    let game = arr[i];
+    if (game.teamID === id) {
+      html += "<tr>";
+      html += "<td>" + game.gameID + "</td>";
+      html += "<td>" + game.playerID + "</td>";
+      html += "<td>" + game.score + "</td>";
+      html += "<td>" + game.gameStateID + "</td>";
+      html += "</tr>";
+    }
+  }
+  html += "</table>";
+}
 
+function handleRowClick(evt) {
+  clearSelections();
+  evt.target.parentElement.classList.add("selected");
+}
+
+function clearSelections() {
+  let trs = document.querySelectorAll("tr");
+  for (let i = 0; i < trs.length; i++) {
+    trs[i].classList.remove("selected");
+  }
+}
+function setButtonState(state) {
+  if (state) {
+    document.querySelector("#viewTeamInfo").removeAttribute("disabled");
+    document.querySelector("#gameRecap").removeAttribute("disabled");
+  } else
+    document
+      .querySelector("#viewTeamInfo")
+      .setAttribute("disabled", "disabled");
+  document.querySelector("#gameRecap").setAttribute("disabled", "disabled");
+}
